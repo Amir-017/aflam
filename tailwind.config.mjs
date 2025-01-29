e2d525a056e71a4d6f795612f335f27b756ad4f3
Your Name <you@example.com>
2025-01-29 20:48:39 +0200

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
